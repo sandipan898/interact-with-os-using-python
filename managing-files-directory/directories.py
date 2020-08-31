5877ad165f4316cdc1f6e0f1bd6758223866f2a5
sandipan898 <sandipan.das898@gmail.com>
2020-08-31 12:28:56 +0530

    adding more files

---
+++ managing-files-directory/directories.py
@@ -0,0 +1,14 @@
+import os
+
+print(os.getcwd())
+
+# os.mkdir("new_dir")
+# os.chdir("new_dir")
+
+print(os.getcwd())
+
+# remove directory
+# os.mkdir("newer_dir")
+# os.rmdir("newer_dir")
+
+print(os.listdir("../managing-files-directory"))
--- managing-files-directory/more-file-info.py
+++ managing-files-directory/more-file-info.py
@@ -1,14 +1,14 @@
+import datetime
 import os
 print(os.path.getsize("renamed_hello_world.py"))
 
-# 
+#
 print(os.path.getmtime("renamed_hello_world.py"))
 
-import datetime
 timestamp = os.path.getmtime("renamed_hello_world.py")
 date_modified = datetime.datetime.fromtimestamp(timestamp)
 print(date_modified)
 
 
-path = os.abspath("renamed_hello_world.py")
+path = os.path.abspath("renamed_hello_world.py")
 print(path)
